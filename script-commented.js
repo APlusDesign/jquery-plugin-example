2eb0439539f630d0bac5ba51dc898d1f1ba9ffa1
simon ilett <simon.ilett@gmail.com>
2013-09-05 09:44:28 +0800

    plugin documentation update

--- script-commented.js
+++ script-commented.js
@@ -1,74 +1,57 @@
-// Document ready
-$(document).ready(function() {
-	// Initialise my plugin on my collection of objects
-	$('div.example-object').pluginName();
-	
-	// Access the plugin for a specific object
-	var myPluginReference = $('#example-object-1').data('pluginName');
-	
-	// Run a method in the plugin for that object
-	myPluginReference.publicMethod(); 
-	
-	// Example showing how private methods can't be accessed 
-	try
-	{
-		myPluginReference.privateMethod(); 
-	}
-	catch(err)
-	{	
-		$("#console").append('Attempted to call private method of <span>'+$('#example-object-1').attr('id') +'</span> but failed! <br />');
-	}
-});
-
-
-/**
- * @author Simon Ilett @ APlusDesign
- * @version 1.0
- * @date 19/03/2013
- */
+// The best way to write a jquery plugin
 (function($){
-	
-	 // 
-	 var PluginName = function(element, options)
-	 {
-		var 
-			obj = element,				  // obj is just a short reference for the jQuery object
-			$this = this,				    // $this is a reference to this, saves scope for use in binds etc
-			settings = $.extend({		// this.settings, or $this.settings
-					aSetting: 'aValue'
-			}, options || {});
-	 
+	//
+	var PluginName = function(element, options)
+	{
+		var
+			obj = element,		// obj is just a short reference for the jQuery argument object
+			$this = this;		// $this is a reference to this, saves scope for use in binds etc
+
+		// this.settings, or $this.settings
+		$this.settings = $.extend({
+			aSetting: 'aValue'
+		}, options || {});
+
+		// Reference to the original object
+		$this.originalObject = obj;
+
 		// Public method - can be called from anywhere once you have the objects plugin reference
-		this.publicMethod = function()
+		$this.publicMethod = function()
 		{
-				$("#console").append('<span>'+ obj.attr('id') + '</span>: public method called! <br>');
+			$("#console").append('<span>'+ obj.attr('id') + '</span>: public method called! <br><br>');
 		};
 
-		// Private method - can only be called from within this object
+		// Private method - can only be called from within this object during execution
 		var privateMethod = function()
 		{
-				$("#console").append('<span>'+ obj.attr('id') + '</span>: private method called! <br>');
+			$("#console").append('<span>'+ obj.attr('id') + '</span>: private method called! <br><br>');
 		};
-	 };
-	 
-	 // Your actual plugin name
-	 $.fn.pluginName = function(options)
-	 {
+
+		/*
+			You don't need a function for init(), you can add run code here after all your methods are defined.
+
+			privateMethod(); // Will work just fine
+		*/
+	};
+
+	// Your actual plugin name
+	$.fn.pluginName = function(options)
+	{
 		return this.each(function()
 		{
 			// obj becomes a jQuery dom object
-				var obj = $(this);
-			
-				// Return if obj already has this plugin instance attached
-				if (obj.data('pluginName')) return;
+			var obj = $(this);
 
-				// Pass obj & options to plugin constructor
-				var pluginName = new PluginName(obj, options);
+			// Return if obj already has this plugin instance attached
+			if (obj.data('pluginName')) return;
 
-				// Store plugin object in jQuery dom obj's data
-				obj.data('pluginName', pluginName);
+			// Pass obj & options to plugin constructor
+			var pluginName = new PluginName(obj, options);
+
+			// Store plugin object in jQuery dom obj's data
+			obj.data('pluginName', pluginName);
 		});
-	 };
+	};
 })(jQuery);
 
 
--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 
 // The best way to write a jquery plugin
 (function($){
-   var PluginName = function(element, options)
-   {
-       	var 
-       		obj = element,				
-       		$this = this,				
-       		settings = $.extend({		
-	           	aSetting: 'aValue'
-	       	}, options || {});
-       
-       	this.publicMethod = function()
-       	{
-           	$("#console").append('<span>'+ obj.attr('id') + '</span>: public method called! <br>');
-       	};
+	var PluginName = function(element, options)
+	{
+		var
+			obj = element,
+			$this = this;
 
-       	var privateMethod = function()
-       	{
-           	$("#console").append('<span>'+ obj.attr('id') + '</span>: private method called! <br>');
-       	};
-   };
+		$this.settings = $.extend({
+			aSetting: 'aValue'
+		}, options || {});
 
-   $.fn.pluginName = function(options)
-   {
-       return this.each(function()
-       {
-           	var obj = $(this);
-           	if (obj.data('pluginName')) return;
-           	var pluginName = new PluginName(obj, options);
-           	obj.data('pluginName', pluginName);
-       });
-   };
+		$this.originalObject = obj;
+
+		$this.publicMethod = function()
+		{
+			$("#console").append('<span>'+ obj.attr('id') + '</span>: public method called! <br><br>');
+		};
+
+		var privateMethod = function()
+		{
+			$("#console").append('<span>'+ obj.attr('id') + '</span>: private method called! <br><br>');
+		};
+	};
+
+	$.fn.pluginName = function(options)
+	{
+		return this.each(function()
+		{
+			var obj = $(this);
+			if (obj.data('pluginName')) return;
+			var pluginName = new PluginName(obj, options);
+			obj.data('pluginName', pluginName);
+		});
+	};
 })(jQuery);
 
 
